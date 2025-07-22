09fec552766a65e9774162600291e844bf34d2aa
Armand Pretorius <80468905+Armand-OW@users.noreply.github.com>
2025-07-22 11:53:11 +0200

    setup of template for demonstration

---
+++ firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Replace the following with your app's Firebase project configuration
+// See: https://firebase.google.com/docs/web/learn-more#config-object
+const firebaseConfig = {
+  // ... Your Configuration Here
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+
+// TODO: Initialise Firebase Authentication and get a reference to the service
